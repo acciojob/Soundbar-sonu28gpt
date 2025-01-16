2ff915fabd768d290f722de3276b2411dd3c11d5
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-16 19:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let btns=document.querySelectorAll('.btn');
+
+let currentAudio=null
+function stopAudio(){
+	if(currentAudio){
+		currentAudio.pause();
+		currentAudio.currentTime=0;
+		currentAudio=null
+	}
+}
+
+for(let el of btns){
+	el.addEventListener('click',()=>{
+		stopAudio();
+		currentAudio=new Audio(`sounds/${el.textContent}`);
+		currentAudio.play();
+		
+	})
+}
